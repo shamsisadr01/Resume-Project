7bd62b50734dbe04ba17b592c7a2e12f59c89248
shamsisadr01 <shamsisadr01@gmail.com>
2024-12-29 18:05:10 +0330

    Admin template transfer

---
+++ Resume.Web/Areas/Admin/Components/SideBarViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Web.Areas.Admin.Components;
+
+public class SideBarViewComponent : ViewComponent
+{
+    #region Method
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View("SideBar");
+    }
+
+    #endregion
+
+}
---
+++ Resume.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Web.Areas.Admin.Controllers;
+
+public class HomeController : AdminBaseController
+{
+    #region Actions
+
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    #endregion
+}
