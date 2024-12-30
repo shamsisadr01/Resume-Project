a476b4e20fc6b0a77b9abac6308d6dcac36cafe1
shamsisadr01 <shamsisadr01@gmail.com>
2024-12-30 11:14:52 +0330

    Transfer template to site

---
+++ Resume.Web/Components/HeaderViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Web.Components;
+
+public class HeaderViewComponent : ViewComponent
+{
+    #region Fields
+
+    #endregion
+
+
+    #region Constructor
+
+    #endregion
+
+    #region Method
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View("Header");
+    }
+
+    #endregion
+}
